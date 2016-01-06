f9bde96ba44ea7024020e6c09775cd104f5aa372
Wyatt Williams <williams.wyatt@gmail.com>
2016-01-06 14:21:20 -0800

    modals are fully operational

--- db/schema.rb
+++ db/schema.rb
@@ -34,9 +34,8 @@ ActiveRecord::Schema.define(version: 20160106195232) do
     t.string   "title"
     t.string   "link"
     t.integer  "user_id"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
-    t.integer  "total_votes"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   add_index "posts", ["user_id"], name: "index_posts_on_user_id", using: :btree
